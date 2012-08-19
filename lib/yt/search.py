5031fd7c7b5d426a019bda4fd6fe38b726833ebe
Will Bickerstaff <will.bickerstaff@gmail.com>
2012-08-19 12:54:20 +0100

    PEP improvements

--- lib/yt/search.py
+++ lib/yt/search.py
@@ -1,4 +1,5 @@
-import urllib, urllib2
+import urllib
+import urllib2
 import time
 from string import Template
 from xml.dom.minidom import parseString
@@ -8,17 +9,14 @@ class Search(object):
     MAXRESULTS = 20
     API = 2
 
-
     def __init__(self):
         self.reset()
-
 
     def reset(self):
         self.hits = 0
         self.start = 1
         self.results = []
         self.totalresults = 10
-
 
     def query(self, url, data={}):
         if self.start < self.totalresults > 0:
@@ -31,15 +29,14 @@ class Search(object):
                 self.start += Search.MAXRESULTS
                 return content
 
-
     def __setTotalResults(self, content):
         if self.start == 1:
-            if len(content.getElementsByTagName('openSearch:totalResults')) > 0:
+            if len(
+                content.getElementsByTagName('openSearch:totalResults')) > 0:
                 self.totalresults = int(content.getElementsByTagName(
                             'openSearch:totalResults')[0].childNodes[0].data)
             else:
                 self.totalresults = 1000
-            
 
     def __hascontent(self, content):
         if content is not None and len(content) > 1:
@@ -50,13 +47,11 @@ class Search(object):
 class PlaylistSearch(Search):
     URL = Template('http://gdata.youtube.com/feeds/api/users/$user/playlists')
 
-
     def __init__(self, **kwargs):
         super(PlaylistSearch, self).__init__()
         self.user = ''
         self.searchterms = ''
         self.__checkkwargs(**kwargs)
-
 
     def __checkkwargs(self, **kwargs):
         for k in kwargs:
@@ -70,34 +65,32 @@ class PlaylistSearch(Search):
                 self.searchterms = kwargs[k]
                 continue
 
-
     def query(self, **kwargs):
         self.__checkkwargs(**kwargs)
         url = PlaylistSearch.URL.substitute({'user': self.user})
 
         if not self.user:
-            raise Valuerror('A valid youtube user must be given to search for '
-                            'playlists.')
+            raise ValueError('A valid youtube user must be given to search '
+                             'for playlists.')
 
         self.hits = 0
         self.results = []
         while ((self.hits < super(PlaylistSearch, self).MAXRESULTS)
                           and (self.totalresults > self.start)):
-              dom = super(PlaylistSearch, self).query(url,
-                                              {'v': Search.API,
-                                               'start-index': self.start,
-                                               'max-results': Search.MAXRESULTS,
-                                               'v': Search.API})
-              if dom is None:
-                  return
-              self.__matchLists(dom)
-              self.hits = len(self.results)
-              if self.hits < super(PlaylistSearch, self).MAXRESULTS:
-                  time.sleep(0.2)  # Lets not hammer the yt servers
+            dom = super(PlaylistSearch, self).query(url,
+                                            {'v': Search.API,
+                                             'start-index': self.start,
+                                             'max-results': Search.MAXRESULTS,
+                                             'v': Search.API})
+            if dom is None:
+                return
+            self.__matchLists(dom)
+            self.hits = len(self.results)
+            if self.hits < super(PlaylistSearch, self).MAXRESULTS:
+                time.sleep(0.2)  # Lets not hammer the yt servers
         if len(self.results) > 0:
             self.results.sort(key=lambda x: x['score'], reverse=True)
             return self.results
-
 
     def __matchLists(self, dom):
         playlists = dom.getElementsByTagName('entry')
@@ -106,13 +99,11 @@ class PlaylistSearch(Search):
             if listdict['score'] > 0:
                 self.results.append(listdict)
 
-
     def __matchSearch(self, playlist):
         listdict = self.__parseList(playlist)
         assert all([len(listdict['title']) > 0, len(listdict['id']) > 0])
         listdict['score'] = self.__score(listdict['title'])
         return listdict
-
 
     def __score(self, title):
         if self.searchterms is None or len(self.searchterms) == 0:
@@ -129,19 +120,17 @@ class PlaylistSearch(Search):
                 multiplier += 1
         return score * multiplier
 
-
     def __createSearchTerms(self):
         """ Join single characters in to the preceeding term """
         terms = self.searchterms.split()
         retTerms = []
         for i, term in enumerate(terms):
-            if i+1 < len(terms) and len(terms[i+1]) < 3:
-                retTerms.append("%s %s" % (term, terms[i+1]))
-                terms.pop(i+1)
+            if i + 1 < len(terms) and len(terms[i + 1]) < 3:
+                retTerms.append("%s %s" % (term, terms[i + 1]))
+                terms.pop(i + 1)
             else:
                 retTerms.append(term)
         return retTerms
-
 
     def __parseList(self, playlist):
         listdict = {}
@@ -155,17 +144,14 @@ class PlaylistSearch(Search):
 class CaptionSearch(Search):
     URL = 'http://www.youtube.com/api/timedtext'
 
-
     def __init__(self, **kwargs):
         super(CaptionSearch, self).__init__()
         self.videoid = None
         self.__checkkwargs(**kwargs)
 
-
     def reset(self):
         self.videoid = None
         super(CaptionSearch, self).reset()
-
 
     def __checkkwargs(self, **kwargs):
         for k in kwargs:
@@ -173,7 +159,6 @@ class CaptionSearch(Search):
             if k in ['id', 'videoid']:
                 self.videoid = kwargs[k]
                 continue
-
 
     def query(self, **kwargs):
         self.__checkkwargs(**kwargs)
@@ -188,7 +173,6 @@ class CaptionSearch(Search):
         self.__parseList(dom)
         return self.results
 
-
     def __parseList(self, queryresult):
         tracks = queryresult.getElementsByTagName('track')
         for track in tracks:
@@ -200,8 +184,7 @@ class CaptionSearch(Search):
 
 
 class GetCaptions(Search):
-    URL =  'http://video.google.com/timedtext'
-
+    URL = 'http://video.google.com/timedtext'
 
     def __init__(self, **kwargs):
         super(GetCaptions, self).__init__()
@@ -221,13 +204,11 @@ class GetCaptions(Search):
                 self.name = kwargs[k]
                 continue
 
-
     def reset(self):
         super(GetCaptions, self).reset()
         self.videoid = None
         self.lang = None
         self.name = None
-
 
     def query(self, **kwargs):
         self.__checkkwargs(**kwargs)
@@ -238,9 +219,10 @@ class GetCaptions(Search):
             raise ValueError('videoid, lang and name must all be '
                              'provided to retrieve captions.')
 
-        dom = super(GetCaptions, self).query(GetCaptions.URL, {'lang': self.lang,
-                                                             'v': self.videoid,
-                                                             'name': self.name})
+        dom = super(GetCaptions, self).query(GetCaptions.URL,
+                                             {'lang': self.lang,
+                                              'v': self.videoid,
+                                              'name': self.name})
         self.__parseList(dom)
         return self.results
 
@@ -251,27 +233,24 @@ class GetCaptions(Search):
             captiontext.append(line.childNodes[0].data)
         self.results = ' '.join(captiontext)
 
+
 class PlaylistVideoSearch(Search):
     URL = Template('http://gdata.youtube.com/feeds/api/playlists/$playlist')
-
 
     def __init__(self, **kwargs):
         super(PlaylistVideoSearch, self).__init__()
         self.reset()
         self.__checkkwargs(**kwargs)
 
-
     def reset(self):
         self.playlistid = None
         super(PlaylistVideoSearch, self).reset()
-
 
     def __checkkwargs(self, **kwargs):
         for k in kwargs:
             k = k.lower()
             if k in ['id', 'playlistid']:
                 self.playlistid = kwargs[k]
-
 
     def query(self, **kwargs):
         url = PlaylistVideoSearch.URL.substitute({'playlist': self.playlistid})
@@ -282,16 +261,15 @@ class PlaylistVideoSearch(Search):
                              'videos.')
         while True:
             dom = super(PlaylistVideoSearch, self).query(url,
-                                             {'v': Search.API,
-                                              'start-index': self.start,
-                                              'max-results': Search.MAXRESULTS})
+                                            {'v': Search.API,
+                                             'start-index': self.start,
+                                             'max-results': Search.MAXRESULTS})
             if dom is None:
                 break
             self.__parseList(dom)
             time.sleep(0.2)  # Lets not hammer the yt servers
         self.results.sort(key=lambda x: x['position'], reverse=False)
         return self.results
-
 
     def __parseList(self, queryresult):
         vids = queryresult.getElementsByTagName('entry')
--- pyPlaylistCaptk.py
+++ pyPlaylistCaptk.py
@@ -1,8 +1,11 @@
-from Tkinter import *
+from Tkinter import (Button, Listbox, Label, Entry, Frame, Scrollbar, DISABLED,
+                     NORMAL, END, N, S, E, W, VERTICAL, HORIZONTAL, SUNKEN,
+                     SINGLE)
 import tkMessageBox
 import string
 import lib.yt.search
 from urllib2 import HTTPError
+
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -14,82 +17,98 @@ class Application(Frame):
         self.results = []
         self.playlists = []
         self.vids = []
-        
 
     def createWidgets(self):
+        self.__userEntryFields()
+        self.__resultArea()
+        self.__buttons()
 
+    def __buttons(self):
+        self.search_button = Button(text="Search", command=self.__search)
+        self.search_button.grid(row=2, column=4,
+                                padx=self.padx, pady=self.pady, sticky=E)
+
+        self.resultSelect = Button(text='OK', state=DISABLED)
+        self.resultSelect.grid(row=5, column=4, sticky=E,
+                               padx=self.padx, pady=self.pady)
+        self.status = Label(text="", bd=1, relief=SUNKEN, anchor=W)
+        self.status.grid(row=9, column=0, columnspan=10, sticky=N + E + S + W)
+        self.__vidButtons()
+        self.__rmVidButtons()
+        self.resultSelect.grid_forget()
+
+    def __userEntryFields(self):
         Label(text="User", anchor=E).grid(row=0, column=0,
-                                          padx=self.padx, pady=self.pady, 
+                                          padx=self.padx, pady=self.pady,
                                           sticky=W)
         self.user_entry = Entry()
-        self.user_entry.grid(row=0,column=1, padx=self.padx, pady=self.pady, 
+        self.user_entry.grid(row=0, column=1, padx=self.padx, pady=self.pady,
                              sticky=W)
-        Label(text="Search terms").grid(row=0, column=3, 
+        Label(text="Search terms").grid(row=0, column=3,
                                         padx=self.padx, pady=self.pady,
                                         sticky=W)
         self.search_terms = Entry()
-        self.search_terms.grid(row=0, column=4, padx=self.padx, pady=self.pady,
+        self.search_terms.grid(row=0, column=4,
+                               padx=self.padx, pady=self.pady,
                                sticky=W)
-        Label(text="playlist id").grid(row=1, column=3, 
-                                       padx=self.padx, pady=self.pady,sticky=W)
+        Label(text="playlist id").grid(row=1, column=3,
+                                       padx=self.padx, pady=self.pady,
+                                       sticky=W)
         self.playlist_id = Entry()
         self.playlist_id.grid(row=1, column=4, padx=self.padx, pady=self.pady,
                               sticky=W)
+
+    def __resultArea(self):
         self.result_label = Label(text="Results")
-        self.result_label.grid(row=2,column=0, padx=self.padx, pady=self.pady,
+        self.result_label.grid(row=2, column=0,
+                               padx=self.padx, pady=self.pady,
                                sticky=W)
-        self.showResultBox()
-        self.search_button = Button(text="Search", command=self.search)
-        self.search_button.grid(row=2, column=4, 
-                                padx=self.padx, pady=self.pady, sticky=E)
+        self.yScroll = Scrollbar(orient=VERTICAL)
+        self.yScroll.grid(row=3, column=5, sticky=N + S)
 
-        
-        self.resultSelect = Button(text='OK', state=DISABLED)
-        self.resultSelect.grid(row=5,column=4, sticky=E, 
-                               padx=self.padx, pady=self.pady)
-        self.status = Label(text="", bd=1, relief=SUNKEN, anchor=W)
-        self.status.grid(row=9, column=0, columnspan=10, sticky=N+E+S+W)
-        self.addvidcommands()
-        self.modtitle.grid_forget()
-        self.getcaps.grid_forget()
-        self.resultSelect.grid_forget()
+        self.xScroll = Scrollbar(orient=HORIZONTAL)
+        self.xScroll.grid(row=4, column=0, sticky=E + W, columnspan=5)
 
-    def search(self):
+        self.listbox = Listbox(xscrollcommand=self.xScroll.set,
+                                yscrollcommand=self.yScroll.set,
+                                selectmode=SINGLE)
+        self.listbox.grid(row=3, column=0, sticky=N + S + E + W, columnspan=5)
+        self.xScroll["command"] = self.listbox.xview
+        self.yScroll["command"] = self.listbox.yview
+
+    def __vidButtons(self):
+        self.modtitle = Button(text='Modify titles', command=self.__modTitles)
+        self.modtitle.grid(row=5, column=0, sticky=W, columnspan=2,
+                           padx=self.padx, pady=self.pady)
+        self.getcaps = Button(text="Get captions", command=self.__getCaptions)
+        self.getcaps.grid(row=5, column=2, columnspan=3, sticky=E,
+                          padx=self.padx, pady=self.pady)
+
+    def __rmVidButtons(self):
+        self.modtitle.grid_remove()
+        self.getcaps.grid_remove()
+
+    def __search(self):
         user = self.user_entry.get()
         playlist = self.playlist_id.get()
         searchterms = self.search_terms.get()
         self.resultSelect.config(state=DISABLED)
-        self.modtitle.grid_forget()
-        self.getcaps.grid_forget()
-        if not self.validparams(user, searchterms, playlist):
+        self.__rmVidButtons()
+        if not self.__validparams(user, searchterms, playlist):
             return False
 
         if len(playlist) > 0:
-            self.searchPlaylist(playlist)
+            self.__searchPlaylist(playlist)
             return
-        
-        self.searchUser(user, searchterms)
-        
-    def showResultBox(self):
-        self.yScroll  =  Scrollbar (orient=VERTICAL )
-        self.yScroll.grid ( row=3, column=5, sticky=N+S, )
 
-        self.xScroll  =  Scrollbar (orient=HORIZONTAL )
-        self.xScroll.grid ( row=4, column=0, sticky=E+W, columnspan=5 )
+        self.__searchUser(user, searchterms)
 
-        self.listbox = Listbox (xscrollcommand=self.xScroll.set,
-                                yscrollcommand=self.yScroll.set,
-                                selectmode=SINGLE   )
-        self.listbox.grid (row=3, column=0, sticky=N+S+E+W, columnspan=5)
-        self.xScroll["command"]  =  self.listbox.xview
-        self.yScroll["command"]  =  self.listbox.yview
+    def __searchPlaylist(self):
+        self.__getvids(self.playlist_id.get())
 
-    def searchPlaylist(self):
-        self.getvids(self.playlist_id.get())
-    
-    def searchUser(self, user, searchterms):
-        self.listbox.delete(0,END)
-        self.status.config(text="Searching for%splaylists by user \"%s\"" %(
+    def __searchUser(self, user, searchterms):
+        self.listbox.delete(0, END)
+        self.status.config(text="Searching for%splaylists by user \"%s\"" % (
                         " \"%s\" " % searchterms if len(searchterms) else " ",
                         user))
         self.status.update_idletasks()
@@ -97,7 +116,7 @@ class Application(Frame):
         self.playlists = []
         try:
             self.playlists = usrsearch.query()
-        except HTTPError as e:
+        except HTTPError:
             self.status.config(text="User %s does not exist at youtube" % user)
             self.status.update_idletasks()
             return
@@ -105,21 +124,25 @@ class Application(Frame):
             self.status.config(text="Search returned no results")
             self.status.update_idletasks()
             return
-        for i,playlist in enumerate(self.playlists):
-            self.listbox.insert(i, playlist['title'])
-        self.listbox.activate(0)
-        self.resultSelect.config(command=self.getVidsFromSelected,
+        self.__populateResults([v['title'] for v in self.playlists])
+        self.resultSelect.config(command=self.__getVidsFromSelected,
                                  state=NORMAL)
         self.status.config(text="")
         self.status.update_idletasks()
-        self.resultSelect.grid(row=5,column=4, sticky=E, 
+        self.resultSelect.grid(row=5, column=4, sticky=E,
                                padx=self.padx, pady=self.pady)
-    
-    def getVidsFromSelected(self):
-        selected = int(self.listbox.curselection()[0])        
-        self.getvids(self.playlists[selected]['id'])
-        
-    def getvids(self, playlistid):
+
+    def __populateResults(self, values):
+        self.listbox.delete(0, END)
+        for i, val in enumerate(values):
+            self.listbox.insert(i, val)
+        self.listbox.activate(0)
+
+    def __getVidsFromSelected(self):
+        selected = int(self.listbox.curselection()[0])
+        self.__getvids(self.playlists[selected]['id'])
+
+    def __getvids(self, playlistid):
         self.resultSelect.grid_forget()
         vidsearch = lib.yt.search.PlaylistVideoSearch(id=playlistid)
         title = playlistid
@@ -128,55 +151,46 @@ class Application(Frame):
                 if playlist['id'] == playlistid:
                     title = playlist['title']
                     break
-        
+
         self.status.config(text="Getting videos for %s" % title)
         self.status.update_idletasks()
-        self.listbox.delete(0,END)
+        self.listbox.delete(0, END)
         self.vids = vidsearch.query()
-        for i,vid in enumerate(self.vids):
-            self.listbox.insert(i,vid['title'])
+        self.__populateResults([v['title'] for v in self.vids])
         self.status.config(text="%d Videos found" % len(self.vids))
         self.status.update_idletasks()
-        self.addvidcommands()
-        
-    def addvidcommands(self):
-        self.modtitle = Button(text='Modify titles', command=self.modTitles)
-        self.modtitle.grid(row=5, column=0, sticky=W, columnspan=2, 
-                           padx=self.padx, pady=self.pady)
-        self.getcaps = Button(text="Get captions", command=self.getCaptions)
-        self.getcaps.grid(row=5, column=2, columnspan=3, sticky=E,
-                          padx=self.padx, pady=self.pady)
-        
-    def getCaptions(self):
+        self.__vidButtons()
+
+    def __getCaptions(self):
         pass
-    
-    def modTitles(self):
+
+    def __modTitles(self):
         pass
-    
-    def validparams(self, user, searchterms, playlist):
+
+    def __validparams(self, user, searchterms, playlist):
         if len(user) == 0 and len(playlist) == 0:
             msg = "Either a valid youtube user or playlist id must be given."
             tkMessageBox.showwarning("missing information", msg)
             return False
 
-        if len(user) > 0 and not self.validstring(user):
+        if len(user) > 0 and not self.__validstring(user):
             msg = "The user given contains invalid characters"
             tkMessageBox.showwarning('Bad user', msg)
             return False
 
-        if len(playlist) > 0 and not self.validstring(playlist):
+        if len(playlist) > 0 and not self.__validstring(playlist):
             msg = "The playlist given contains invalid characters"
             tkMessageBox.showwarning('Bad playlist', msg)
             return False
-        
-        if len(searchterms) > 0 and not self.validstring(searchterms, True):
+
+        if len(searchterms) > 0 and not self.__validstring(searchterms, True):
             msg = "The search terms given contain invalid characters"
-            tkMessageBox.showwarning('Bad search', msg)        
+            tkMessageBox.showwarning('Bad search', msg)
             return False
 
         return True
 
-    def validstring(self, s, spacechar=False):
+    def __validstring(self, s, spacechar=False):
         validchars = string.letters + string.digits + string.punctuation
         if spacechar:
             validchars += ' '
